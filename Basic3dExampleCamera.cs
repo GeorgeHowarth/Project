75dd4a3cb0e48e3d33f30d19b105b8345c277aac
GeorgeHowarth <92313075+GeorgeHowarth@users.noreply.github.com>
2021-11-04 10:24:30 +0000

    Add files via upload

---
+++ Basic3dExampleCamera.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CamTest
+{
+    public class Basic3dExampleCamera
+    {
+        private GraphicsDevice graphicsDevice = null;
+        private GameWindow gameWindow = null;
+        public float sensitivity = 20f;
+        bool invertControls = false;
+        private float unitsPerSecond = 5;
+        private float unitsPerSecondSprint = 10;
+        private float gravity = 0;
+        public float BulletVelocity = 15f;
+        
+
+        public float FieldOfView = MathHelper.PiOver4;
+        public float ShortRender = 0.01f;
+        public float LongRender = 200f;        //render distance
+
+        public int xValue = 400;
+        public int yValue = 240;
+
+        public double totalTime = 0;
+
+        public bool moving = false;
+        public bool jumping = false;
+        public bool crouching = false;
+        public bool sprinting = false;
+
+        public Vector2 PositionXZ;
+
+        /// <summary>
+        /// this serves as the cameras up for fixed cameras this might not change at all ever.
+        /// </summary>
+        private Vector3 Up = Vector3.Up;
+        /// <summary>
+        /// this serves as the cameras world orientation 
+        /// it holds all orientational values and is used to move the camera properly thru the world space as well.
+        /// </summary>
+        public Matrix camerasWorld = Matrix.Identity;
+        /// <summary>
+        /// The view matrix is created from the cameras world matrixs but it has special propertys.
+        /// Using create look at to create this matrix you move from the world space into the view space.
+        /// If you are working on world objects you should not take individual elements from this to directly operate on world matrix components.
+        /// As well the multiplication of a view matrix by a world matrix moves the resulting matrix into view space itself.
+        /// </summary>
+        private Matrix viewMatrix = Matrix.Identity;
+        /// <summary>
+        /// Constructs the camera.
+        /// </summary>
+        public Basic3dExampleCamera(GraphicsDevice gfxDevice, GameWindow window)
+        {
+            graphicsDevice = gfxDevice;
+            gameWindow = window;
+            ReCreateWorldAndView();
+        }
+        /// <summary>
+        /// Determines how the camera behaves true for fixed false for free.
+        /// </summary>
+        //public bool IsFixed { get; set; } = true;
+        /// <summary>
+        /// Gets or sets the the camera's position in the world.
+        /// </summary>
+        public Vector3 Position
+        {
+            set
+            {
+                camerasWorld.Translation = value;
+                // since we know here that a change has occured to the cameras world orientations we can update the view matrix.
+                ReCreateWorldAndView();
+            }
+            get { return camerasWorld.Translation; }
+        }
+
+        /// <summary>
+        /// Gets or Sets the direction the camera is looking at in the world.
+        /// </summary>
+        public Vector3 LookAtDirection
+        {
+            set
+            {
+                camerasWorld = Matrix.CreateWorld(camerasWorld.Translation, value, Up);
+                // since we know here that a change has occured to the cameras world orientations we can update the view matrix.
+                ReCreateWorldAndView();
+            }
+            get { return camerasWorld.Forward * 10; }
+        }
+
+        /// <summary>
+        /// Sets a positional target in the world to look at.
+        /// </summary>
+        public Vector3 Target
+        {
+            set
+            {
+                camerasWorld = Matrix.CreateWorld(camerasWorld.Translation, Vector3.Normalize(value - camerasWorld.Translation), Up);
+                // since we know here that a change has occured to the cameras world orientations we can update the view matrix.
+                ReCreateWorldAndView();
+            }
+        }
+
+        /// <summary>
+        /// When the cameras position or orientation changes we call this to ensure, the cameras world matrix is orthanormal and to update the view matrix.
+        /// </summary>
+        private void ReCreateWorldAndView()
+        {
+          camerasWorld = Matrix.CreateWorld(camerasWorld.Translation, camerasWorld.Forward, Up);
+          viewMatrix = Matrix.CreateLookAt(camerasWorld.Translation, camerasWorld.Forward + camerasWorld.Translation, camerasWorld.Up);
+        }
+        /// <summary>
+        /// Gets the view matrix.
+        /// </summary>
+        public Matrix ViewMatrix
+        {
+            get
+            {
+                return viewMatrix;
+            }
+        }
+        /// <summary>
+        /// Gets the projection matrix.
+        /// </summary>
+        public Matrix ProjectionMatrix
+        {
+            get
+            {
+                float aspectRatio = graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
+                return Matrix.CreatePerspectiveFieldOfView(FieldOfView, aspectRatio, ShortRender, LongRender);
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            // mouse controls
+            RotateLeftOrRight(gameTime, -MouseMovement().X);         //mouse look left/right
+            RotateUpOrDown(gameTime, -MouseMovement().Y);            // mouse look up/down
+            Mouse.SetPosition(400, 240);
+
+            // movement controls
+            Position = new Vector3(Position.X, Position.Y + gravity, Position.Z); //gravity
+
+            if (Position.Y < 2)
+                Position = new Vector3(Position.X,2,Position.Z); //placeholder for collision on ground
+
+            //jump code start
+            if (kstate.IsKeyDown(Keys.Space)== true && !jumping)
+            {
+                jumping = true;
+                gravity = 0.15f;
+            }
+            if (Position.Y > 2)
+            {
+                gravity = gravity - 0.005f;
+            }
+            else
+                jumping = false;
+
+
+
+            //jump code end //needs fixing
+
+            moving = false;
+            if (kstate.IsKeyDown(Keys.W))   //forward
+            {
+                MoveForward(gameTime);
+                moving = true;
+            }
+            
+            if (kstate.IsKeyDown(Keys.S) == true) //back 
+            {
+                MoveBackward(gameTime);
+                moving = true;
+            }
+            
+            if (kstate.IsKeyDown(Keys.A) == true) //left
+            {
+                MoveLeft(gameTime);
+                moving = true;
+            }
+            
+            if (kstate.IsKeyDown(Keys.D) == true) //right 
+            {
+                MoveRight(gameTime);
+                moving = true;
+            }
+           
+            if (kstate.IsKeyDown(Keys.LeftShift) == true)  //sprint 
+                sprinting = true;
+            
+            if (kstate.IsKeyDown(Keys.LeftShift) == false) // not sprinting 
+                sprinting = false;
+            
+            if (!sprinting && !crouching)
+                unitsPerSecond = 5;
+
+            if (kstate.IsKeyDown(Keys.LeftControl) == true) //crouch
+            {
+                if(!jumping && Position.Y <= 3f)
+                {
+                    crouching = true;
+                    Position = new Vector3(Position.X, 1.5f, Position.Z);   // crouch on ground at y = 1.5
+                    unitsPerSecond = 2;
+                }
+            }
+            if (kstate.IsKeyDown(Keys.LeftControl) == false) //not crouching
+            {
+                crouching = false;
+                if (!jumping && !sprinting && Position.Y == 1.5)
+                {
+                    Position = new Vector3(Position.X, 2, Position.Z);      //normal movement height at y=2
+                    unitsPerSecond = 5;                                     //normal run speed = 5
+                }
+            }
+        }
+        public Vector2 MouseMovement()               //mouse is set to x400 y240 every update, this tracks how far from these coordinates it is
+        {
+            int x = 0;
+            int y = 0;
+
+            if (Mouse.GetState().Position.X != 400)
+                x = 400 - Mouse.GetState().Position.X;
+
+            if (Mouse.GetState().Position.Y != 240)
+                y = 240 - Mouse.GetState().Position.Y;
+
+            Vector2 Movement = new Vector2(x, y);
+            return Movement;
+        }
+
+        public void MoveForward(GameTime gameTime)
+        {
+            if (sprinting)
+                unitsPerSecond = unitsPerSecondSprint;
+            Position += (camerasWorld.Forward * unitsPerSecond) *(float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        }
+        public void MoveBackward(GameTime gameTime)
+        {
+            if (sprinting)
+                unitsPerSecond = unitsPerSecondSprint;
+            Position += (camerasWorld.Backward * unitsPerSecond) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+        public void MoveLeft(GameTime gameTime)
+        {
+            if (sprinting)
+                unitsPerSecond = unitsPerSecondSprint;
+            Position += (camerasWorld.Left * unitsPerSecond) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+        public void MoveRight(GameTime gameTime)
+        {
+            if (sprinting)
+                unitsPerSecond = unitsPerSecondSprint;
+            Position += (camerasWorld.Right * unitsPerSecond) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public void RotateLeftOrRight(GameTime gameTime, float amount) //mouse look left/right
+        {
+            if (invertControls)
+                amount = -amount;
+            var radians = amount * -sensitivity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Matrix matrix = Matrix.CreateFromAxisAngle(camerasWorld.Up, MathHelper.ToRadians(radians));
+            LookAtDirection = Vector3.TransformNormal(LookAtDirection, matrix);
+            
+            ReCreateWorldAndView();
+        }
+        public void RotateUpOrDown(GameTime gameTime, float amount) //mouse look up/down
+        {
+            if (invertControls)
+                amount = -amount;
+            var radians = amount * -sensitivity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Matrix matrix = Matrix.CreateFromAxisAngle(camerasWorld.Right, MathHelper.ToRadians(radians));
+            LookAtDirection = Vector3.TransformNormal(LookAtDirection, matrix);
+
+            ReCreateWorldAndView();
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+/// <summary>
+/// This is the main type for your game.
+/// </summary>
+namespace CamTest
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        BasicEffect beffect;
+        Basic3dExampleCamera cam;
+        Matrix camWorldObjectToVisualize = Matrix.Identity;
+        
+        Grid3dOrientation worldGrid = new Grid3dOrientation(20, 20, .01f);
+        OrientationLines orientationLines = new OrientationLines(.2f, 1f);
+
+        Texture2D textureForward;
+        Texture2D textureRight;
+        Texture2D textureUp;
+        bool canshoot = true;
+        int timeuntilnextbullet = 0;
+        int uprecoil;
+        Random rnd = new Random();
+
+        private BasicEffect basicEffect; 
+        //shooting
+
+
+        List<Bullet> bulletList = new List<Bullet>();
+        Target[] Buttons = new Target[3];
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Target startButton = new Target();
+            //creates the start button
+            startButton.IsVisible = true;
+            startButton.Position = new Vector3(0,3,-10);
+            startButton.model = Content.Load<Model>(@"start");
+            Buttons[0] = startButton;
+
+            Target settingsButton = new Target();
+            //creates the settings button
+            settingsButton.IsVisible = true;
+            settingsButton.Position = new Vector3(-10, 3, -10);
+            settingsButton.model = Content.Load<Model>(@"settings");
+            Buttons[1] = settingsButton;
+
+            Target quitButton = new Target();
+            //creates the quit button
+            quitButton.IsVisible = true;
+            quitButton.Position = new Vector3(10, 3, -10);
+            quitButton.model = Content.Load<Model>(@"quit");
+            Buttons[2] = quitButton;
+
+            //Target room = new Target();
+            //room.IsVisible = true;
+            //room.Position = new Vector3(0, 0, 0);
+            //room.model = Content.Load<Model>(@"room");
+            //Buttons[3] = room;
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            beffect = new BasicEffect(GraphicsDevice);
+            cam = new Basic3dExampleCamera(GraphicsDevice, Window);
+            cam.Position = new Vector3(0, 1, 10);
+            cam.Target = Vector3.Zero;
+
+            textureForward = CreateCheckerBoard(GraphicsDevice, 20, 20, Color.Red, Color.Red);      // creates red X CheckerBoard
+            textureRight = CreateCheckerBoard(GraphicsDevice, 20, 20, Color.Yellow, Color.Yellow);  // creates yellow Y CheckerBoard
+            textureUp = CreateCheckerBoard(GraphicsDevice, 20, 20, Color.Blue, Color.Blue);         // creates blue Z CheckerBoard
+
+            beffect.VertexColorEnabled = true;
+            beffect.TextureEnabled = true;
+            beffect.World = Matrix.Identity;
+            beffect.Projection = cam.ProjectionMatrix;
+
+            //crosshair = Content.Load<Texture2D>(@"crosshair");
+
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.View = Matrix.CreateLookAt(new Vector3(50, 50, 50), new Vector3(0, 0, 0), Vector3.Up);
+            basicEffect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);
+            IsMouseVisible = false;
+
+    }
+
+        protected override void UnloadContent()
+        {
+            textureForward.Dispose();
+            textureRight.Dispose();
+            textureUp.Dispose();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            MouseState state = Mouse.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //if (Keyboard.GetState().IsKeyDown(Keys.F12))
+            //    graphics.ToggleFullScreen();
+
+            cam.Update(gameTime);
+            beffect.View = cam.ViewMatrix;
+
+            camWorldObjectToVisualize = Matrix.CreateWorld(Vector3.One, cam.LookAtDirection, Vector3.Up);
+            //shooting
+            base.Update(gameTime);
+
+            if (state.LeftButton == ButtonState.Pressed && canshoot)
+            {
+                CreateNewBullet();
+                canshoot = false;             //controls the fire rate
+                timeuntilnextbullet = 0;
+                //start recoil code
+                double random = rnd.Next(-2, 2);
+                uprecoil = -2;
+                if(cam.moving) // if moving, recoil is increased
+                {
+                    random = random * 2;
+                    uprecoil = -6;
+                }
+                if (cam.crouching)    //if crouching recoil is reduced
+                {
+                    random = random / 2;
+                    uprecoil = -1;
+                }
+                else if (cam.jumping)  //if jumping recoil is massively increased
+                {
+                    random = random * 3;
+                    uprecoil = -10;
+                }
+
+                int leftorright = Convert.ToInt32(Math.Round(random));
+                cam.RotateUpOrDown(gameTime, uprecoil);       //upwards recoil is 5 when standing
+                cam.RotateLeftOrRight(gameTime, leftorright);     //right recoil is random between 2 to the left and 2 to the right but changed based on movement
+                //end recoil code
+            }
+            if (!canshoot)
+            {
+                timeuntilnextbullet++;
+                if (timeuntilnextbullet >= 5)  //firerate
+                    canshoot = true;
+            }
+
+            UpdateObjects(gameTime);
+
+            foreach(Bullet bullet in bulletList)
+            {
+                //start
+                if (bullet.bulletPosition.X > -1 && bullet.bulletPosition.X < 3 && bullet.bulletPosition.Y > 1 && bullet.bulletPosition.Y < 4 && bullet.Position.Z > -12 && bullet.Position.Z < -8 && bullet.IsVisible) //placeholder for collision
+                {
+                    cam.Position = new Vector3(100, 0, 100); // moves the player when "start" button hit
+                    bullet.IsVisible = false;
+                }
+                //settings
+                if (bullet.bulletPosition.X > -11 && bullet.bulletPosition.X < -7 && bullet.bulletPosition.Y > 1 && bullet.bulletPosition.Y < 4 && bullet.Position.Z > -12 && bullet.Position.Z < -8 && bullet.IsVisible) //placeholder for collision
+                {
+                    Buttons[1].IsVisible = !Buttons[1].IsVisible;
+                    bullet.IsVisible = false;
+                }
+                //quit
+                if (bullet.bulletPosition.X > 9 && bullet.bulletPosition.X < 13 && bullet.bulletPosition.Y > 1 && bullet.bulletPosition.Y < 4 && bullet.Position.Z > -12 && bullet.Position.Z < -8 && bullet.IsVisible) //placeholder for collision
+                {
+                    Exit(); //closes game when quit button hit
+                }
+            }
+        }
+
+        public void UpdateObjects(GameTime gt) 
+        {
+            foreach (Bullet bullet in bulletList)               //this piece of code handles how each bullet moves
+            {
+                if (bullet.shooting)
+                {
+                    if (bullet.bulletCount >= 20)
+                        bullet.shooting = false;
+
+                    bullet.bulletsWorld = Matrix.CreateTranslation(bullet.Position);
+                    bullet.bulletsWorld = Matrix.CreateWorld(bullet.bulletsWorld.Translation, bullet.bulletsWorld.Forward, Vector3.Up);
+                    bullet.Position += (bullet.Direction * bullet.Velocity) * (float)gt.ElapsedGameTime.TotalSeconds;
+                    bullet.bulletCount++;
+                }
+                else
+                {
+                    bullet.IsVisible = false;
+                }
+            }
+            foreach (Target button in Buttons)
+            {
+                button.targetsWorld = Matrix.CreateWorld(Matrix.CreateTranslation(button.Position).Translation, button.targetsWorld.Forward, Vector3.Up);
+            }
+        }
+
+        public void CreateNewBullet()
+        {
+            Bullet newBullet = new Bullet();                         //creates a new bullet using the bullet class
+            newBullet.Position = cam.Position;
+            newBullet.shooting = true;
+            newBullet.Direction = cam.camerasWorld.Forward;
+            newBullet.IsVisible = true;
+            newBullet.model = Content.Load<Model>(@"Sphere");
+
+            bulletList.Add(newBullet);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DeepSkyBlue);
+
+            GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+
+            worldGrid.DrawWithBasicEffect(GraphicsDevice, beffect, Matrix.Identity, 30f, textureForward, textureRight, textureUp);
+
+            orientationLines.DrawWithBasicEffect(GraphicsDevice, beffect, Matrix.Identity);
+            base.Draw(gameTime);
+
+            //shooting
+
+            foreach(Target button in Buttons)
+            {
+                if(button.IsVisible)
+                foreach (ModelMesh mesh in button.model.Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+                        effect.World = button.targetsWorld;
+                        effect.View = cam.ViewMatrix;
+                        effect.Projection = cam.ProjectionMatrix;
+                    }
+                    mesh.Draw();
+                }
+            }
+            foreach (Bullet bullet in bulletList)
+            {
+                if (bullet.IsVisible)
+                foreach (ModelMesh mesh in bullet.model.Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+                        effect.World = bullet.bulletsWorld;
+                        effect.View = cam.ViewMatrix;
+                        effect.Projection = cam.ProjectionMatrix;
+                    }
+                    mesh.Draw();
+                }
+            }
+        }
+
+        //creates the checkerboard pattern
+        public static Texture2D CreateCheckerBoard(GraphicsDevice device, int w, int h, Color c0, Color c1)
+        {
+            Color[] data = new Color[w * h];
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    int index = y * w + x;
+                    Color c = c0;
+                    if ((y % 2 == 0))
+                    {
+                        if ((x % 2 == 0))
+                            c = c0;
+                        else
+                            c = c1;
+                    }
+                    else
+                    {
+                        if ((x % 2 == 0))
+                            c = c1;
+                        else
+                            c = c0;
+                    }
+                    data[index] = c;
+                }
+            }
+            Texture2D tex = new Texture2D(device,w,h);
+            tex.SetData<Color>(data);
+            return tex;
+        }
+    }
+}
+
